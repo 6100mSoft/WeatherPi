c0445a5945bf0bfaaa08dec7149079c57a628b03
6100m <77129415+6100m@users.noreply.github.com>
2022-02-28 16:12:19 -0700

    More features 1

--- client.py
+++ client.py
@@ -54,8 +54,10 @@ if __name__ == "__main__":
     n = 3
     i = 0
     if len(argv) != 3:
-        lcd.printline(0, "Teddiursa Client")
-        lcd.printline(1, "client.py user" + "#" + "channel")
+        lcd.printline(
+            0, "Teddiursa Client")
+        lcd.printline(
+            1, "client.py user" + "#" + "channel")
         lcd.printline(2, "Initilization Status:")
         lcd.printline(3, "Init Complete!")
         exit(0)
@@ -85,47 +87,75 @@ if __name__ == "__main__":
             else:
                 n = n - 3
                 clr()
+            f1 = open("conf1.txt", "r")
+            f2 = open("conf2.txt", "r")
+            f3 = open("conf3.txt", "r")
+            f4 = open("conf4.txt", "r")
+            f5 = open("conf5.txt", "r")
+            dict_data = [
+                f1.read()
+                f2.read()
+                f3.read()
+                f4.read()
+                f5.read()
+            ]
             if "No Ident response" in res or authNotSent:
                 ins.send("USER", "{} * * :{}".format(usr, usr))
                 ins.send("NICK", usr)
-                ins.send(
-                    "PRIVMSG",
-                    f"{ch} :"
-                    + f"Client Node: {seed} | Teddiursa IRC Client: Missing ident response, retrying...",
-                )
+                if dict_data[1] = 0:
+                    ins.send(
+                        "PRIVMSG",
+                        f"{ch} :"
+                        + f"Client Node: {seed} | Teddiursa IRC Client: Missing ident response, retrying...",
+                    )
+                else:
+                    print("type 0 --- flag 0 flashed")
                 authNotSent = False
             if "376" in res:
                 ins.join()
-                ins.send(
-                    "PRIVMSG",
-                    f"{ch} :"
-                    + f"Client Node: {seed} | Teddiursa IRC Client: User has joined. DL Teddiursa IRC Client @ https://github.com/6100m/Teddiursa-IRC-Client/",
-                )
+                if dict_data[1] = 0:
+                    ins.send(
+                        "PRIVMSG",
+                        f"{ch} :"
+                        + f"Client Node: {seed} | Teddiursa IRC Client: User has joined. DL Teddiursa IRC Client @ https://github.com/6100m/Teddiursa-IRC-Client/",
+                    )
+                else:
+                    print("type 1 --- flag 0 flashed")
             if "433" in res:
                 ins.send("USER", "{} * * :{}".format("_" + usr, "_" + usr))
                 ins.send("NICK", "_" + usr)
-                ins.send(
-                    "PRIVMSG",
-                    f"{ch} :"
-                    + f"Client Node: {seed} | Teddiursa IRC Client: Got code 433.",
-                )
+                if dict_data[2] = 0:
+                    ins.send(
+                        "PRIVMSG",
+                        f"{ch} :"
+                        + f"Client Node: {seed} | Teddiursa IRC Client: Got code 433.",
+                    )
+                else:
+                    print("type 2 --- flag 0 flashed")
             if "PING" in res:
                 ins.send("PONG", ":" + res.split(":")[1])
-                ins.send(
-                    "PRIVMSG",
-                    f"{ch} :"
-                    + f"Client Node: {seed} | Teddiursa IRC Client: Testing ping....",
-                )
+                if dict_data[2] = 0:
+                    ins.send(
+                        "PRIVMSG",
+                        f"{ch} :"
+                        + f"Client Node: {seed} | Teddiursa IRC Client: Testing ping....",
+                    )
+                else:
+                    print("type 3 --- flag 0 flashed")
             if "366" in res:
                 flg = True
-                ins.send(
-                    "PRIVMSG",
-                    f"{ch} :"
-                    + f"Client Node: {seed} | Teddiursa IRC Client: Got code 366.",
+                if dict_data[2] = 0:
+                    ins.send(
+                        "PRIVMSG",
+                        f"{ch} :"
+                        + f"Client Node: {seed} | Teddiursa IRC Client: Got code 366.",
                 )
+                else:
+                    print("type 4 --- flag 0 flashed")
         a_int = 0
         while cmd != "/quit":
-            cmd = input("< {}> ".format(usr)).strip()
+            cmd = input("< {}> ".format(
+                usr)).strip()
             if a_int <= 2:
                 a_int = a_int + 1
             else:
