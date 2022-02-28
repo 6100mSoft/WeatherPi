70700ae01b862067559a41743bf3d9a4bb63eea0
6100m <77129415+6100m@users.noreply.github.com>
2022-02-27 20:57:51 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,84 +1,110 @@
-from sys import argv
-from threading import Thread
-import liquidcrystal_i2c
+#!/usr/bin/env python
+# by bl4de | github.com/bl4de | twitter.com/_bl4de | hackerone.com/bl4de
 import socket
-def clr():
-    for x in range(0,3):
-        lcd.printline(x,"")
-def log():
-    lcd=liquidcrystal_i2c.LiquidCrystal_I2C(0x27,1,numlines=4)
-    if ins.get():
-        msg=ins.get().strip().split(":")
-        if i<=3:
-            lcd.printline(i,"< {}> {}".format(msg[1].split("!")[0],msg[2].strip()))
+import threading
+import sys
+import liquidcrystal_i2c
+def usage():
+    print("IRC simple Python client | by bl4de | github.com/bl4de | twitter.com/_bl4de | hackerone.com/bl4de\n")
+    print("$ ./irc_client.py USERNAME CHANNEL\n")
+    print("where: USERNAME - your username, CHANNEL - channel you'd like to join (eg. channelname or #channelname)")
+def channel(channel):
+    if channel.startswith("#") == False:
+        return "#" + channel
+    return channel
+# helper function used as thread target
+def print_response():
+    lcd = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 1, numlines=4)
+    resp = client.get_response()
+    if resp:
+        msg = resp.strip().split(":")
+        if data <= 3:
+            lcd.printline(data,"< {}> {}".format(msg[1].split("!")[0], msg[2].strip()))
         else:
-            clr()
-class Client:
-    def __init__(self,usr,ch,srv="irc.freenode.net",dev=6667):
-        self.usr=usr
-        self.srv=srv
-        self.dev=dev
-        self.ch=ch
-    def con(self):
-        self.con=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.con.connect((self.srv,self.dev))
-    def get(self):
-        return self.con.recv(512).decode("utf-8")
-    def send(self,cmd,msg):
-        self.con.send("{} {}\r\n".format(cmd,msg).encode("utf-8"))
-    def msgr(self,msg):
-        cmd="PRIVMSG {}".format(self.ch)
-        self.send(cmd,":"+msg)
-    def join(self):
-        self.send("JOIN",self.ch)
-if __name__=="__main__":
-    lcd=liquidcrystal_i2c.LiquidCrystal_I2C(0x27,1,numlines=4)
-    n=3
-    i=0
-    if len(argv)!=3:
-        lcd.printline(0,"bl4de IRC Client mod")
-        lcd.printline(1,"client.py user" + "#" + "channel")
-        lcd.printline(2,"Initilization Status:")
-        lcd.printline(3,"Init Complete!")
+            for x in range(0, 3):
+                lcd.printline(x, "")
+        if data <= 2:
+            data = data + 1
+            for x in range(0, 3):
+                lcd.printline(x, "")
+        else:
+            data = data - 3
+            for y in range(0, 3):
+                lcd.printline(y, "")
+class IRCSimpleClient:
+    def __init__(self, username, channel, server="irc.freenode.net", port=6667):
+        self.username = username
+        self.server = server
+        self.port = port
+        self.channel = channel
+    def connect(self):
+        self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.conn.connect((self.server, self.port))
+    def get_response(self):
+        return self.conn.recv(512).decode("utf-8")
+    def send_cmd(self, cmd, message):
+        command = "{} {}\r\n".format(cmd, message).encode("utf-8")
+        self.conn.send(command)
+    def send_message_to_channel(self, message):
+        command = "PRIVMSG {}".format(self.channel)
+        message = ":" + message
+        self.send_cmd(command, message)
+    def join_channel(self):
+        cmd = "JOIN"
+        channel = self.channel
+        self.send_cmd(cmd, channel)
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        usage()
         exit(0)
     else:
-        usr=argv[1]
-        ch=f"#{argv[2]}"
-        cmd=""
-        flg=False
-        ins=Client(usr,ch)
-        ins.con()
-        lcd.printline(2,"Bootup Status:")
-        lcd.printline(3,"Bootup Complete!")
-        # Proper registration implementation by my friend epicness @ github.com/3picness
-        # Thanks! :3
-        authNotSent = True
-        while(flg==False):
-            res=ins.get()
-            if n<=2:
-                n=n+1
-                clr()
-            else:
-                n=n-3
-                clr()
-            if "No Ident response" in res or authNotSent:
-                ins.send("USER","{} * * :{}".format(usr,usr))
-                ins.send("NICK",usr)
-                authNotSent = False
-            if "376" in res:
-                ins.join()
-            if "433" in res:
-                ins.send("USER","{} * * :{}".format("_"+usr,"_"+usr))
-                ins.send("NICK","_"+usr)
-            if "PING" in res:
-                ins.send("PONG", ":"+res.split(":")[1])
-            if "366" in res:
-                flag=True
-        while(cmd != "/quit"):
-            if input("< {}> ".format(usr)).strip()=="/quit":
-                ins.send("QUIT", "Good bye!")
-            ins.msgr(cmd)
-            run=Thread(target=log)
-            run.daemon=True
-            clr()
-            run.start()
+        username = sys.argv[1]
+        channel = channel(sys.argv[2])
+    data = 0
+    lcd = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 1, numlines=4)
+    cmd = ""
+    joined = False
+    client = IRCSimpleClient(username, channel)
+    client.connect()
+    int_data = 0
+    while(joined == False):
+        resp = client.get_response()
+        if data <= 2:
+            data = data + 1
+            for x in range(0, 3):
+                lcd.printline(x, "")
+        else:
+            data = data - 3
+            for y in range(0, 3):
+                lcd.printline(y, "")
+        print(res.strip())
+        #lcd.printline(int_data, resp.strip())
+        if "No Ident response" in resp:
+            client.send_cmd("NICK", username)
+            client.send_cmd(
+                "USER", "{} * * :{}".format(username, username))
+        # we're accepted, now let's join the channel!
+        if "376" in resp:
+            client.join_channel()
+        # username already in use? try to use username with _
+        if "433" in resp:
+            username = "_" + username
+            client.send_cmd(
+                "USER", "{} * * :{}".format(username, username))
+            client.send_cmd("NICK", username)
+        # if PING send PONG with name of the server
+        if "PING" in resp:
+            client.send_cmd("PONG", ":" + resp.split(":")[1])
+        # we've joined
+        if "366" in resp:
+            joined = True
+    while(cmd != "/quit"):
+        cmd = input("< {}> ".format(username)).strip()
+        if cmd == "/quit":
+            client.send_cmd("QUIT", "Good bye!")
+        client.send_message_to_channel(cmd)
+        # socket conn.receive blocks the program until a response is received
+        # to prevent blocking program execution, receive should be threaded
+        response_thread = threading.Thread(target=print_response)
+        response_thread.daemon = True
+        response_thread.start()
