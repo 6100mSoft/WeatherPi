99a760fb63208518decdebbaa63af2b5fa4852d8
6100m <77129415+6100m@users.noreply.github.com>
2022-02-27 20:51:19 -0700

    Update client.py

--- client.py
+++ client.py
@@ -25,7 +25,6 @@ class Client:
     def get(self):
         return self.con.recv(512).decode("utf-8")
     def send(self,cmd,msg,n):
-        lcd.printline(n,"{} {}\r\n".format(cmd,msg).encode("utf-8"))
         self.con.send("{} {}\r\n".format(cmd,msg).encode("utf-8"))
     def msgr(self,msg):
         cmd="PRIVMSG {}".format(self.ch)
@@ -49,8 +48,8 @@ if __name__=="__main__":
         flg=False
         ins=Client(usr,ch)
         ins.con()
-        #lcd.printline(2,"Bootup Status:")
-        #lcd.printline(3,"Bootup Complete!")
+        lcd.printline(2,"Bootup Status:")
+        lcd.printline(3,"Bootup Complete!")
         # Proper registration implementation by my friend epicness @ github.com/3picness
         # Thanks! :3
         authNotSent = True
