b7ba6bda295ad3f6adbe5ba19f6304f5ea36fc43
6100m <77129415+6100m@users.noreply.github.com>
2022-03-15 21:52:39 -0400

    Update client.py

--- client.py
+++ client.py
@@ -101,7 +101,7 @@ if __name__ == "__main__":
             else:
                 n = n - 3
                 clr()
-            with open(str(get()) + "debug.json", "r") as f:
+            with open(str(get()) + "/debug.json", "r") as f:
                 config = load(f)
             if "No Ident response" in res or authNotSent:
                 ins.send("USER", "{} * * :{}".format(usr, usr))
