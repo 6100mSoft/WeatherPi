22e0a1bcc0775ff1297090c05500f7690c3b49ed
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-10-02 14:29:05 -0600

    Cleanup

--- client.py
+++ client.py
@@ -12,13 +12,12 @@ def ClearScreen():
     for x in range(0, 3): lcd.printline(x, "")
  
 def LogToScreen(msg, i):
-    lcd = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 1, numlines=4)
-        lcd.printline(i, "< {}> {}".format(
-            msg[1].split("!")[0], msg[2].strip()))
+        liquidcrystal_i2c.LiquidCrystal_I2C(
+            0x27, 1, numlines=4).printline(i, "< {}> {}".format(
+                msg[1].split("!")[0], msg[2].strip()))
 
 def PrintTimeConstantly():
-    with open("./keys.json", "rb") as f:
-            config = json.load(f)
+    with open("./keys.json", "rb") as f: config = json.load(f)
     while config["key1_main"] == config['key1_mirror']:
         LogToScreen(0, now.strftime("%H:%M:%S"))
         time.sleep(96)
@@ -27,34 +26,37 @@ def PrintTimeConstantly():
 def PrintWeatherConstantly():
     # Based off of 
     # https://www.tutorialspoint.com/find-current-weather-of-any-city-using-openweathermap-api-in-python
-    with open("./api.json", "rb") as api_conf:
-        api_config = json.load(api_conf)
-    with open("./location.json", "rb") as loc_conf:
-        locator_config = json.load(loc_conf)
-    with open("./keys.json", "rb") as key_conf:
-        key_config = json.load(key_conf)
+    with open("./api.json", "rb") as api_conf: api_config = json.load(api_conf)
+    with open("./location.json", "rb") as loc_conf: locator_config = json.load(loc_conf)
+    with open("./keys.json", "rb") as key_conf: key_config = json.load(key_conf)
     while config["key2_main"] == config['key2_mirror']:
         response = requests.get(
-            f"https://api.openweathermap.org/data/2.5/weather?q={locator_config['CITY']}&appid={api_config['API_KEY']}"
-        )
+            f"https://api.openweathermap.org/data/2.5/weather?q={locator_config['CITY']}&appid={api_config['API_KEY']}")
         if response.status_code == 200:
-            main = response.json()['main']
-            LogToScreen(1, main['temp'])
+            LogToScreen(1, response.json()['main']['temp'])
             time.sleep(1)
             for x in range(0, 1): lcd.printline(i, "")
 
-if __name__ == "__main__":
-    lcd = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 1, numlines=4)
-    lcd.printline(0, "WeatherPi Client")
-    lcd.printline(1, "Type start and press enter to start!")
-    lcd.printline(2, "Initilization Status:")
-    lcd.printline(3, "Init Complete!")
-    ClearScreen()
-    for num in range(0, 1): lcd.printline(num, "")
-    t1 = threading.Thread(target=PrintWeatherConstantly)
-    t2 = threading.Thread(target=PrintTimeConstantly)
-    lcd.printline(2, "Bootup Status:")
-    lcd.printline(3, "Bootup is beginning")
-    t1.Start()
-    t2.Start()
+def BootupIsComplete():
     lcd.printline(3, "Complete! | WeatherPi OS v0.1")
+
+if __name__ == "__main__":
+    with open("./keys.json", "rb") as key_configuration: key_json = json.load(key_configuration)
+    while key_json['key3_main'] == key_json['key3_mirror']:
+        lcd = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 1, numlines=4)
+        lcd.printline(0, "WeatherPi Client")
+        lcd.printline(1, "Type start and press enter to start!")
+        lcd.printline(2, "Initilization Status:")
+        lcd.printline(3, "Init Complete!")
+        ClearScreen()
+        t1 = threading.Thread(target=PrintWeatherConstantly)
+        t2 = threading.Thread(target=PrintTimeConstantly)
+        t3 = threading.Thread(target=BootupIsComplete)
+        lcd.printline(2, "Bootup Status:")
+        lcd.printline(3, "Bootup is beginning")
+        t1.Start()
+        t2.Start()
+        t3.Start()
+        t3.Join()
+        t2.Join()
+        t1.Join()
