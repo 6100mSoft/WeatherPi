391a77347e1f929870d3a9852a16e1dbbf55dedd
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-10-30 14:12:24 -0600

    40 line version

--- client.py
+++ client.py
@@ -9,9 +9,9 @@ def TimePrint(key, dup):
         LiquidCrystal_I2C(0x27, 1, numlines=4).printline(0, "%H:%M:%S")
 
 
-def TempPrint(lst, resp=response.json()["main"]["temp"]):
+def TempPrint(lst, a_dict, resp=response.json()["main"]["temp"]):
     while lst[0]["key2_main"] == lst[0]["key2_mirror"]:
-        if get(data[3]).status_code == 200:
+        if get(a_dict[4]).status_code == 200:
             LiquidCrystal_I2C(0x27, 1, numlines=4).printline(1, resp)
 
 
@@ -20,15 +20,16 @@ if __name__ == "__main__":
         load(open("./keys.json", "rb")),
         load(open("./api.json", "rb")),
         load(open("./location.json", "rb")),
-        f"{data[1]['URL']}?q={data[2]['CITY']}&appid={data[1]['API_KEY']}",
     ]
-    for num in range(0, 3), data in [
+    a_dict = [
         "WeatherPi Client",
         "Type start and press enter to start!",
         "Initilization Status:",
         "Init Complete!",
-    ]:
-        LiquidCrystal_I2C(0x27, 1, numlines=4).printline(num, data)
+        f"{lst[1]['URL']}?q={lst[2]['CITY']}&appid={lst[1]['API_KEY']}",
+    ]
+    for n in range(0, 3):
+        LiquidCrystal_I2C(0x27, 1, numlines=4).printline(n, a_dict[n])
     if input("Listening....") == "start":
         while key["key3_main"] == key["key3_mirror"]:
             for num in range(2, 3), data in ["Bootup Complete!", "WeatherPi OS v0.2"]:
