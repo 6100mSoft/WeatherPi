62c0329ecc04ec68247245ab4aafaeefeeb01aac
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-10-30 13:45:50 -0600

    Reformat with black

--- client.py
+++ client.py
@@ -2,29 +2,48 @@ from liquidcrystal_i2c import LiquidCrystal_I2C
 from time import sleep
 from json import load
 from threading import Thread
-from os import _exit
 from requests import get
+from os import _exit
 
-def LogToScreen(line_num, msg_data): LiquidCrystal_I2C(0x27, 1, numlines=4).printline(line_num, msg_data)
-    
-def PrintTimeConstantly(main, mirror): while main == mirror: LogToScreen(0, "%H:%M:%S"))
 
-def PrintWeatherConstantly():
-    a_dict = [load(open("./api.json", "rb")), load(open("./location.json", "rb")), load(open("./keys.json", "rb"))]
-    while key["key2_main"] == key['key2_mirror']:
-        if get(f"{a_dict[0]['API_URL']}?q={a_dict[1]['CITY']}&appid={a_dict[2]['APIKEY']}").status_code == 200:
-        	LogToScreen(1, response.json()['main']['temp'])
+def TimePrint(key, dup):
+    while key == dup:
+        LiquidCrystal_I2C(0x27, 1, numlines=4).printline(0, "%H:%M:%S")
+
+
+def TempPrint(data, resp=response.json()["main"]["temp"]):
+    while key["key2_main"] == key["key2_mirror"]:
+        if get(data[2]).status_code == 200:
+            LiquidCrystal_I2C(0x27, 1, numlines=4).printline(1, resp)
+
 
 if __name__ == "__main__":
-    with open("./keys.json", "rb") as key_json: key = load(key_json)
-    while key['key3_main'] == key['key3_mirror']: for num in range(0, 3), data in ["WeatherPi Client",
-	"Type start and press enter to start!", "Initilization Status:", "Init Complete!"]: LogToScreen(num, data)
-        if input("Listening....") == "start":
-	    for num in range(2, 3), data in ["Bootup Status:", "Bootup is beginning"]: LogToScreen(num, data)
+    listing = [
+        load(open("./keys.json", "rb")),
+        [
+            load(open("./api.json", "rb")),
+            load(open("./location.json", "rb")),
+            load(open("./keys.json", "rb")),
+        ],
+        f"{data[1][0]['URL']}?q={data[1][1]['CITY']}&appid={data[1][0]['API_KEY']}",
+    ]
+    for num in range(0, 3), data in [
+        "WeatherPi Client",
+        "Type start and press enter to start!",
+        "Initilization Status:",
+        "Init Complete!",
+    ]:
+        LiquidCrystal_I2C(0x27, 1, numlines=4).printline(num, data)
+    if input("Listening....") == "start":
+        while key["key3_main"] == key["key3_mirror"]:
+            for num in range(2, 3), data in ["Bootup Status:", "Bootup is beginning"]:
+                LiquidCrystal_I2C(0x27, 1, numlines=4).printline(num, data)
             sleep(24)
-            for num in range(2, 3), data in ["Bootup Status:", "Complete! | WeatherPi OS v0.1"]: LogToScreen(num, data)
-            Thread(target=PrintWeatherConstantly).Start()
-            Thread(target=PrintTimeConstantly, args=(key['key1_main'], key['key1_mirror'])).Start()
-            Thread(target=PrintWeatherConstantly).Join()
-            Thread(target=PrintTimeConstantly, aargs=(key['key1_main'], key['key1_mirror'])).Join()
-       else: _exit(24)
+            for num in range(2, 3), data in ["Bootup Complete!", "WeatherPi OS v0.1"]:
+                LiquidCrystal_I2C(0x27, 1, numlines=4).printline(num, data)
+            Thread(target=TempPrint, args=(listing)).Start()
+            Thread(target=TimePrint, args=(["key1_main"], key["key1_mirror"])).Start()
+            Thread(target=TempPrint, args=(listing)).Join()
+            Thread(target=TimePrint, args=(key["key1_main"], key["key1_mirror"])).Join()
+    else:
+        _exit(24)
