b94e095f8833dd114016e4ac90592bcc20bd832c
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-10-30 13:58:16 -0600

    Simplify

--- client.py
+++ client.py
@@ -1,9 +1,7 @@
 from liquidcrystal_i2c import LiquidCrystal_I2C
-from time import sleep
 from json import load
 from threading import Thread
 from requests import get
-from os import _exit
 
 
 def TimePrint(key, dup):
@@ -11,8 +9,8 @@ def TimePrint(key, dup):
         LiquidCrystal_I2C(0x27, 1, numlines=4).printline(0, "%H:%M:%S")
 
 
-def TempPrint(data, resp=response.json()["main"]["temp"]):
-    while key["key2_main"] == key["key2_mirror"]:
+def TempPrint(lst, resp=response.json()["main"]["temp"]):
+    while lst[0]["key2_main"] == lst[0]["key2_mirror"]:
         if get(data[2]).status_code == 200:
             LiquidCrystal_I2C(0x27, 1, numlines=4).printline(1, resp)
 
@@ -20,11 +18,9 @@ def TempPrint(data, resp=response.json()["main"]["temp"]):
 if __name__ == "__main__":
     lst = [
         load(open("./keys.json", "rb")),
-        [
-            load(open("./api.json", "rb")),
-            load(open("./location.json", "rb")),
-        ],
-        f"{data[1][0]['URL']}?q={data[1][1]['CITY']}&appid={data[1][0]['API_KEY']}",
+        load(open("./api.json", "rb")),
+        load(open("./location.json", "rb")),
+        f"{data[1]['URL']}?q={data[2]['CITY']}&appid={data[1]['API_KEY']}",
     ]
     for num in range(0, 3), data in [
         "WeatherPi Client",
@@ -35,14 +31,9 @@ if __name__ == "__main__":
         LiquidCrystal_I2C(0x27, 1, numlines=4).printline(num, data)
     if input("Listening....") == "start":
         while key["key3_main"] == key["key3_mirror"]:
-            for num in range(2, 3), data in ["Bootup Status:", "Bootup is beginning"]:
-                LiquidCrystal_I2C(0x27, 1, numlines=4).printline(num, data)
-            sleep(24)
             for num in range(2, 3), data in ["Bootup Complete!", "WeatherPi OS v0.1"]:
                 LiquidCrystal_I2C(0x27, 1, numlines=4).printline(num, data)
             Thread(target=TempPrint, args=(lst)).Start()
             Thread(target=TimePrint, args=(lst[0]["key1"], lst[0]["key1_dup"])).Start()
             Thread(target=TempPrint, args=(lst)).Join()
             Thread(target=TimePrint, args=(lst[0]["key1"], lst[0]["key1_dup"])).Join()
-    else:
-        _exit(24)
